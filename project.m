a97576f744adae7d9cb6270b71dffa7b2e242a01
maxxies <ahiamadzormaxwell7@gmail.com>
2023-07-22 16:59:08 +0000

    Change input to rgb

--- project.m
+++ project.m
@@ -1,26 +1,21 @@
 % Step 1: Image Preprocessing
-image = imread('../../../Desktop/FOLDERS/SCHOOL/FINAL YEAR/2ND SEMESTER/DSP/PROJECT/test01.jpg'); % Load the image
+image = imread('./test01.jpg'); % Load the image
 image = im2double(image); % Convert to double precision
-hsvImage = rgb2hsv(image); % Convert to HSV color space
 
+% Step 2: Color-Based Segmentation
+targetColor = [0, 1, 1]; % Red color (in RGB format)
+tolerance = 0.3; % Tolerance value for color matching
 
-% Step 2: Color Thresholding
-colorName = input('Enter color name: ', 's'); % Prompt user for color name
+reshapedImage = reshape(image, [], 3); % Reshape image to a 2D matrix (pixels x RGB channels)
 
-% Convert color name to HSV thresholds
-[hueThreshold, saturationThreshold, valueThreshold] = colorNameToThresholds(colorName);
+% Find the indices of pixels within the tolerance range of the target color
+distances = sqrt(sum((reshapedImage - targetColor).^2, 2));
+mask = distances <= tolerance;
 
-% Check if the color name is valid
-if isempty(hueThreshold) || isempty(saturationThreshold) || isempty(valueThreshold)
-    error('Invalid color name.');
-end
-
-mask = (hsvImage(:,:,1) >= hueThreshold(1) & hsvImage(:,:,1) <= hueThreshold(2)) ...
-    & (hsvImage(:,:,2) >= saturationThreshold(1) & hsvImage(:,:,2) <= saturationThreshold(2)) ...
-    & (hsvImage(:,:,3) >= valueThreshold(1) & hsvImage(:,:,3) <= valueThreshold(2));
 
 % Step 3: Image Segmentation
-labeledImage = logical(mask); % Label connected components
+labeledImage = reshape(mask, size(image, 1), size(image, 2)); % Reshape back to original image size
+labeledImage = logical(labeledImage); % Label connected components
 regions = regionprops(labeledImage, 'Area', 'BoundingBox', 'Centroid');
 
 % Step 4: Shape Analysis
@@ -31,8 +26,10 @@ filteredRegions = regions([regions.Area] > minAreaThreshold); % Filter regions b
 maskedImage = image; % Create a copy of the original image
 maskedImage(repmat(~mask, [1, 1, size(image, 3)])) = 0; % Set non-red regions to zero
 
+
 % Step 6: Highlight or Extract the Selected Shapes
-highlightedShapes = insertShape(image, 'rectangle', cat(1, filteredRegions.BoundingBox), 'LineWidth', 3, 'Color', 'white');
+highlightedShapes = insertShape(image, 'rectangle', cat(1, filteredRegions.BoundingBox), 'LineWidth', 50, 'Color', 'black');
+
 
 % Display the original image, the masked image, and the highlighted shapes
 figure;
